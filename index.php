baf938caa33c86c583c13fe7dfb6b700977c937d
SamoSadlaker <samkosadlak@gmail.com>
2021-03-16 09:21:26 +0100

    Main container & logo

--- index.php
+++ index.php
@@ -18,7 +18,18 @@
 <body>
 
   <main>
+    <header>
+      <img src="assets/img/logo.svg" alt="Logo">
+      <h1>Status page</h1>
+    </header>
     
+    <div class="content">
+
+    </div>
+
+    <footer>
+      <a href="portfolio.samo"><i class='bx bxs-home'></i> Back to home.</a>
+    </footer>
   </main>
   
 </body>
