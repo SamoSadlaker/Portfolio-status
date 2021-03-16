3448141a09ec160722580a7dd4ce282d134dc814
SamoSadlaker <samkosadlak@gmail.com>
2021-03-16 17:21:14 +0100

    Metatags

--- index.php
+++ index.php
@@ -6,8 +6,38 @@ $status = new StatusController();
 <html lang="sk">
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="title" content="Status | SamoSadlaker's Portfolio">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="My name is SamoSadlaker, I am a web developer. This is my portfolio where you can find information about me and my projects.">
+  <meta name="keywords" content="Portfolio, Web Development, Frontend, Backend, PHP, HTML, CSS, Java, Spigot, Developer, SamoSadlaker">
+  <meta name="author" content="SamoSadlaker">
+  <meta name="robots" content="noindex">
+
+  <!-- Open Graph Meta Tags -->
+  <meta property="og:title" content="SamoSadlaker's Portfolio" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://www.samosadlaker.eu/" />
+  <meta property="og:image" content="https://samosadlaker.eu/assets/img/logo.svg" />
+  <meta property="og:description" content="My name is SamoSadlaker, I am a web developer. This is my portfolio where you can find information about me and my projects." />
+  <meta property="og:site_name" content="Portfolio, SamoSadlaker" />
+
+  <!-- Twitter Meta Tags -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://samosadlaker.eu/">
+  <meta property="twitter:title" content="SamoSadlaker's Portfolio">
+  <meta property="twitter:description" content="My name is SamoSadlaker, I am a web developer. This is my portfolio where you can find information about me and my projects.">
+  <meta property="twitter:image" content="https://samosadlaker.eu/assets/img/logo.svg">
+
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+  <link rel="manifest" href="assets/img/favicons/site.webmanifest">
+  <link rel="mask-icon" href="assets/img/favicons/safari-pinned-tab.svg" color="#008bf8">
+  <link rel="shortcut icon" href="assets/img/favicons/favicon.ico">
+  <meta name="msapplication-TileColor" content="#151515">
+  <meta name="msapplication-config" content="assets/img/favicons/browserconfig.xml">
+  <meta name="theme-color" content="#151515">
 
   <!-- Page Title -->
   <title>Status | SamoSadlaker's Portfolio</title>
